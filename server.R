e5ff3cc5249f0610b95e709888e43f939eb90ab6
Michael DeWitt <dewittme@wfu.edu>
2018-10-28 12:56:07 -0400

    Add App

---
+++ server.R
@@ -0,0 +1,512 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+# general stuff needed outside of app -------------------------------------
+
+library(shiny)
+library(leaflet)
+library(tidyverse)
+library(sf)
+library(ggmap)
+library(rgdal)
+library(raster)
+library(htmltools)
+library(lme4)
+library(DT)
+
+# add ward shape files
+wards <- st_read("data/wards_20150327.shp")
+
+wards_2 <- st_transform(wards, crs = 4326)
+
+wards_2 %>% 
+  mutate(
+    lon = map_dbl(geometry, ~st_centroid(.x)[[1]]),
+    lat = map_dbl(geometry, ~st_centroid(.x)[[2]])
+  )->wards_3
+
+content <- wards_3 %>%
+  dplyr::select(Ward, lon, lat) %>% 
+  mutate(Ward = as.character(Ward))
+# bring in crime data
+crime_dat <- read_csv("data/geocoded_incidents_with_census_data.csv") %>% 
+  mutate(day_of_week = lubridate::wday(incident_date, label = TRUE),
+         month = lubridate::month(incident_date, label = TRUE))
+
+all_winston <- crime_dat %>% 
+  group_by(Ward) %>% 
+  summarise(pctpoverty = mean(pctpov, na.rm = TRUE),
+            pctwhite = mean(pctwhite, na.rm = TRUE)) %>% 
+  filter(!is.na(Ward)) %>% 
+  mutate_if(is.numeric, round, 0)
+
+# start the server --------------------------------------------------------
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output, session) {
+     
+  # WSPD Logo
+  
+  output$wspd_logo <- renderUI({
+    tags$img(src= "http://www.cityofws.org/portals/0/images/marketing-and-communications/w-s-logos/W-S_logo_580.jpg",
+             width="250", height="250")
+  })
+  
+  output$census_logo <- renderUI({
+    tags$img(src= "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/U.S._Census_Bureau_logo_post-2011.svg/800px-U.S._Census_Bureau_logo_post-2011.svg.png", 
+             width="600", height="250")
+  })
+  
+  # Get Reactive Data Based on Filters
+  get_data <- reactive({
+    get_data <- crime_dat %>% filter(Ward == input$ward, month == input$month)
+  })
+  
+  crime_dat_reduced <- reactive({
+    crime_dat_reduced <- crime_dat %>% 
+      dplyr::select(case, call_type, address, Ward, indcident_date, incident_time)
+  })
+  
+  # Descriptive Stats Tab
+  # Total Top 10
+  event_total <- function(){
+    crime_dat %>% 
+      group_by(call_type) %>%
+      summarise(n = n()) %>% 
+      top_n(10) %>% 
+      ggplot(aes(reorder(call_type,n), n))+
+      geom_col()+
+      coord_flip()+
+      labs(
+        title = paste0("Top 10 Incidents for Winston-Salem NC in ", input$month),
+        caption = "Data from Winston-Salem Police Department",
+        x = NULL
+      )+
+      theme_minimal()+
+      theme(plot.title = element_text(size =16))
+  }
+  
+  output$event_total <- renderPlot({
+    event_total()
+  })
+  # Graph 2
+  map_by_month <- function(){
+    get_data() %>% 
+      group_by(call_type) %>% 
+      summarise(n = n()) %>% 
+      top_n(10) %>% 
+      ggplot(aes(reorder(call_type,n), n))+
+      geom_col()+
+      coord_flip()+
+      labs(
+        title = paste0("Top 10 Incidents for the ", input$ward, " in ", input$month),
+        caption = "Data from Winston-Salem Police Department",
+        x= NULL
+      )+
+      theme_minimal()+
+      theme(plot.title = element_text(size =16))
+  }
+  
+  output$map_by_month <- renderPlot({
+    map_by_month()
+  })
+  
+  # Download Graphs
+  output$download_descriptive <- downloadHandler(
+    filename = function() {
+      paste0(Sys.Date(),"WSPD-Descriptive-Analysis.pdf")
+    },
+    content = function(file) {
+      pdf(file)
+      print( map_by_month() )
+      print(event_total())
+      print(demog_points())
+      dev.off()
+    }
+  )
+  
+  
+  # Demographics Table
+  
+  output$demog_table <- renderTable({
+    if(input$demog){
+      print(all_winston %>% 
+              rename(`Pct Poverty` = pctpoverty,
+                     `Pct White` = pctwhite))
+    } else{
+      NULL
+    }
+  }
+  )
+  
+  demog_points <- function(){
+    crime_dat %>% 
+      ggplot(aes(pctpov, pctwhite))+
+      geom_point()+
+      theme_minimal()+
+      labs(
+        title = "Demographic Information",
+        subtitle = "Comparison of Poverty and White Populations",
+        caption = "Data from American Community Survey and \n Winston Salem Police Department"
+      )
+  }
+  
+  # Demographics LineChart
+  
+  output$demog_points <- renderPlot({
+    if(input$demog){
+      demog_points()
+    } else{
+      NULL
+    }
+  })
+  
+  # Make Reduced Data Set
+  
+  brushed_points <- function(){
+    brushedPoints(crime_dat, input$plot_brush)
+  }
+  
+  #Brushed Points
+  output$brush_info <- renderDataTable(
+    brushed_points(), extensions = "Buttons",
+    options = list(dom = "Bfrtip",pageLength = 50,
+                   buttons = c("copy", "csv", "excel", "pdf", "print"))
+    
+  )
+
+# dates and times ---------------------------------------------------------
+
+  # Days and Time Tabs
+  
+  # Plot 1 function
+  day_of_week_plot <- function(){
+    crime_dat %>% 
+      group_by(day_of_week) %>% 
+      summarise(n = n()) %>% 
+      ggplot(aes(day_of_week, n))+
+      geom_col()+
+      labs(
+        title = "Number of Incidents by Day of the Week",
+        caption = "Data from Winston-Salem Police Department",
+        x=NULL
+      )+
+      theme(plot.title = element_text(size =16))
+  }
+  # Make Plot
+  output$day_of_week <- renderPlot(
+    day_of_week_plot()
+  )
+  # Make Download
+  output$download_day_of_week <- downloadHandler(
+    filename = function() { paste("day_of_week", '.png', sep='') },
+    content = function(file) {
+      ggsave(file, plot = day_of_week_plot(), device = "png")
+    }
+  )
+  
+  # Make Day of Week Plot
+  
+  day_of_week_trend <- function(){
+    crime_dat %>% 
+      group_by(month, day_of_week) %>% 
+      summarise(n = n()) %>% 
+      ggplot(aes(day_of_week, n, color = month, group = month))+
+      geom_line()+
+      theme_minimal()+
+      labs(
+        title = "Incidents over Time",
+        color = "Month",
+        caption = "Data from Winston-Salem Police Department",
+        x = NULL
+      )+
+      theme(plot.title = element_text(size =16))
+  }
+  
+  output$day_of_week_trend <- renderPlot(
+    day_of_week_trend()
+  )
+  
+  # Make Month Figures Plot
+  
+  month_fig <-function(){
+    crime_dat %>% 
+      group_by(month) %>% 
+      summarise(n = n()) %>% 
+      ggplot(aes(month, n))+
+      geom_col()+
+      theme_minimal()+
+      labs(
+        title = "Number of Incidents by Month",
+        caption = "Data from Winston-Salem Police Department",
+        x = NULL
+      )+
+      theme(plot.title = element_text(size =16))
+  }
+  
+  output$month_fig <- renderPlot(
+    month_fig()
+  )
+  
+  # Facet by Ward by Month Figures Plot
+  
+  ward_month_fig <-function(){
+    crime_dat %>% 
+      group_by(Ward, month) %>% 
+      summarise(n = n()) %>% 
+      ggplot(aes(month, n))+
+      geom_line(aes(group=1))+
+      facet_wrap(~Ward)+
+      theme_minimal()+
+      labs(
+        title = "Number of Incidents by Month by Ward",
+        caption = "Data from Winston-Salem Police Department",
+        x = NULL
+      )+
+      theme(plot.title = element_text(size =16))
+  }
+  
+  output$ward_month_fig <- renderPlot(
+    ward_month_fig()
+  )
+  
+  # Event By Time
+  
+  plot_time <- function(){
+    crime_dat %>% 
+      mutate(hour = lubridate::hour(incident_time),
+             hour = factor(hour)) %>% 
+      count(incident_date, hour) %>% 
+      ggplot(aes(hour, n))+
+      geom_boxplot()+
+      geom_jitter(alpha = .1)+
+      theme_minimal()+
+      labs(
+        title = "Distribution of Incidents by Hour of Day",
+        subtitle = "Grouped by Day and Hour",
+        caption = "Data from Winston- Salem Police Department",
+        y = "Count of Incidents",
+        x = "Hour of The Day"
+      )
+  }
+  
+  # Plot Time
+  output$plot_time<- renderPlot({
+    plot_time()
+  })
+  
+  
+  # Date Zoom
+  day_data <- reactive({
+    crime_dat %>% 
+      mutate(day_num = lubridate::day(incident_date)) %>% 
+      filter(month == input$time_month,
+             day_num == input$date_slide)
+  })
+  
+  day_plot <- function(){
+    day_data() %>% 
+      group_by(call_type) %>% 
+      summarise( n = n()) %>% 
+      top_n(10) %>% 
+      ggplot(aes(reorder(call_type,n), n))+
+      geom_col()+
+      labs(
+        x = NULL,
+        title = paste0("Top 10 Incident Types in Winston-Salem  on ",input$time_month, " ",input$date_slide),
+        caption = "Data from the Winston-Salem Police Department"
+      )+
+      theme_minimal()+
+      coord_flip()
+  }
+  
+  output$day_plot <- renderPlot({
+    day_plot()
+  })
+  
+  observe({
+    dat <- crime_dat %>% 
+      mutate(day_num = lubridate::day(incident_date)) %>% 
+      filter(month == input$time_month)
+    max_day <- max(dat$day_num)
+    updateSliderInput(session, "date_slide", max = max_day)
+  })
+  
+  # Download All for the tab
+  output$download_all_date_time <- downloadHandler(
+    filename = function() {
+      paste0(Sys.Date(),"WSPD-Date-Time-Analysis.pdf")
+    },
+    content = function(file) {
+      pdf(file)
+      print( day_of_week_plot() )
+      print( day_of_week_trend() )  
+      print( month_fig() )
+      print(ward_month_fig())
+      print(plot_time())
+      print(day_plot())
+      dev.off()
+    }
+  )
+  # Download Data for this tab
+  output$download_date_time_csv <- downloadHandler(
+    filename = "crime_by_ward_date_time.csv",
+    content = function(file) {
+      write.csv(crime_dat_reduced(), file)
+    },
+    contentType = "text/csv"
+  )
+
+# leaflet geo mapping -----------------------------------------------------
+
+  # Leaflet Mapping for Geolocator
+  events_in_bounds <- reactive({
+    if (is.null(input$map_bounds))
+      return(crime_dat[FALSE,])
+    bounds <- input$map_bounds
+    latRng <- range(bounds$north, bounds$south)
+    lngRng <- range(bounds$east, bounds$west)
+    
+    subset(crime_dat,
+           lat >= latRng[1] & lat <= latRng[2] &
+             lon >= lngRng[1] & lon <= lngRng[2]) %>% 
+      dplyr::select(case:incident_time)
+  })
+    
+    output$map <- renderLeaflet({
+      leaflet(wards_3) %>% 
+        addTiles() %>% 
+        addPolygons(opacity = .5, fillOpacity = .1) %>% 
+        addMarkers(lng = ~lon,lat = ~lat)
+    })
+    
+    observe({
+      if(input$points){
+        leafletProxy("map", data = crime_dat %>% filter(month == input$map_month)) %>% 
+          leaflet::clearGroup(group = "crimedots") %>% 
+          addCircles(lng = ~lon,lat = ~lat, group = "crimedots")
+      } else{
+        leafletProxy("map", data = wards_3) %>% 
+          clearGroup("crimedots")
+      }
+      
+    })
+    
+    
+    library(DT)
+    
+    output$table <- renderDataTable(
+      events_in_bounds(), extensions = "Buttons",
+      options = list(dom = "Bfrtip",pageLength = 50,
+                     buttons = c("copy", "csv", "excel", "pdf", "print")))
+    
+    output$download_csv <- downloadHandler(
+      filename = "crime_by_ward.csv",
+      content = function(file) {
+        write.csv(events_in_bounds(), file)
+      },
+      contentType = "text/csv"
+    )
+    
+
+# predictive modeling -----------------------------------------------------
+
+    # Set Up Equation
+    output$equation <- renderUI({
+      main_effects <- input$predictors
+      p(
+        withMathJax(
+      if(!input$addgroupeffects){
+        if(length(main_effects) == 2){
+          sprintf("$$inv log(Y_{%s}) = \\beta_{%s} + \\beta_{%s} + \\alpha$$",
+                  input$call, main_effects[[1]], main_effects[[2]])
+        } else if (length(main_effects) == 3){
+          sprintf("$$inv log(Y_{%s}) = \\beta_{%s} + \\beta_{%s} +  \\beta_{%s}+ \\alpha$$",
+                  input$call, main_effects[[1]], main_effects[[2]], main_effects[[3]])
+        } else{
+          sprintf("$$inv log(Y_{%s}) = \\beta_{%s} + \\alpha$$",input$call,
+                  main_effects[[1]])
+        }
+                      
+      } else {
+        if(length(input$groups)==2){
+          if(length(input$predictors) == 3){
+            sprintf("$$inv log(Y_{%s}) = \\beta_{%s} + \\beta_{%s} + \\beta_{%s} + \\delta_{%s}+\\gamma_{%s}+\\alpha$$",
+                    input$call, main_effects[[1]], main_effects[[2]], main_effects[[3]],
+                    input$groups[[1]], input$groups[[2]])
+          } else if (length(input$predictors) == 2){
+            sprintf("$$inv log(Y_{%s}) = \\beta_{%s} + \\beta_{%s} + \\delta_{%s}+\\gamma_{%s}+\\alpha$$",
+                    input$call, main_effects[[1]], main_effects[[2]],
+                    input$groups[[1]], input$groups[[2]])
+          } else {
+            sprintf("$$inv log(Y_{%s}) = \\beta_{%s} + \\delta_{%s} + \\gamma_{%s} + \\alpha$$",
+                    input$call, main_effects[[1]], 
+                    input$groups[[1]], input$groups[[2]])
+          }} else{
+            if(length(input$predictors) == 3){
+              sprintf("$$inv log(Y_{%s}) = \\beta_{%s} + \\beta_{%s} + \\beta_{%s} + \\delta_{%s}+\\alpha$$",
+                      input$call, main_effects[[1]], main_effects[[2]], 
+                      main_effects[[3]],input$groups[[1]])
+            } else if (length(input$predictors) == 2){
+              sprintf("$$inv log(Y_{%s}) = \\beta_{%s} + \\beta_{%s} + \\delta_{%s}+\\alpha$$",
+                      input$call, main_effects[[1]], main_effects[[2]],input$groups[[1]])
+            } else {
+              sprintf("$$inv log(Y_{%s}) = \\beta_{%s}  + \\delta_{%s}+\\alpha$$",
+                      input$call, main_effects[[1]], input$groups[[1]])
+            }
+        }
+      }
+        ))
+    }
+    )
+    
+    # Create the Desired Dataframe for Modeling
+    model_data <- reactive({
+      crime_dat %>% 
+        mutate(hour = lubridate::hour(incident_time)) %>% 
+        dplyr::select(input$predictors, input$groups, call_type) %>% 
+        mutate(y = ifelse(call_type == input$call,1,0))
+    })
+    
+    fit <- reactive({
+      if(!input$addgroupeffects){
+        fit <- glm(paste0("y~", paste0(input$predictors, collapse = "+")),
+                   data = model_data(), family = "binomial")
+      } else if (length(input$groups)==1){
+        fit <-lmer(paste0("y~", paste0(input$predictors, collapse = "+"),
+                          paste0("+ (1|",input$groups,")")),
+                   data = model_data(), family = "binomial")
+      } else if (length(input$groups)==2){
+        fit <-lmer(paste0("y~", paste0(input$predictors, collapse = "+"),
+                          paste0("+ (1|",input$groups[[1]],")","+ (1|",input$groups[[2]],")")),
+                   data = model_data(), family = "binomial")
+      } else{
+        NULL
+      }
+      
+    })
+    
+    # Output model fit
+    output$model_fit <- renderPrint({
+      summary(fit())
+    })
+    # Output model fit
+    output$model_graph <- renderPlot({
+      sjPlot::plot_model(fit(), show.values = TRUE, value.offset = .3, 
+                         title ="Parameter Estimates")+
+        theme(plot.title = element_text(size =16))
+    })
+    
+    # Output the dataframe for modeling for inspection
+    output$data_to_model <- renderDataTable({
+      model_data()
+    }
+    )
+    
+})
---
+++ ui.R
@@ -0,0 +1,202 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinydashboard)
+library(leaflet)
+library(tidyverse)
+library(shinyjs)
+
+crime_dat <- read_csv("data/geocoded_incidents_with_census_data.csv") %>% 
+  mutate(day_of_week = lubridate::wday(incident_date, label = TRUE),
+         month = lubridate::month(incident_date, label = TRUE))
+
+some_link <-"google.com"
+
+# Define UI for application that draws a histogram
+shinydashboard::dashboardPage(skin = "yellow",
+                              
+  #add title
+  dashboardHeader(title = "Winston-Salem Crime Tracker", titleWidth = 800),
+  
+  #Add sidebar elements
+  dashboardSidebar(sidebarMenu(
+    menuItem("About", tabName = "about", icon = icon("archive")),
+    menuItem(
+      "Descriptive Stats",
+      tabName =  "descr",
+      icon = icon("user")
+    ),
+    menuItem(
+      "Days and Time",
+      tabName = "daytime",
+      icon = icon("calendar")
+    ),
+    menuItem("Geolocator", tabName = "geolocate", icon = icon("globe")),
+    menuItem("Prediction", tabName = "preds", icon = icon("paper-plane"))
+  )),
+  # Add Body Elements
+  
+  dashboardBody(
+    tabItems(
+      # First Tab
+      tabItem(tabName = "about",
+              fluidRow(
+                withMathJax(),
+                h1("What this Application Does"),
+                "The purpose of this applications is to complete some analysis on 
+                   different events in which the police depart of Winston Salem is Involved.
+                   This analysis includes using data from the police department itself to 
+                   make summary statistics, review time and date of incidents, as well as
+                   position these events within the citizens and the wards themselves.",
+                h2("Method"),
+                "Data were scraped for each day since 2018-01-01 from the Winston-Salem
+                Police Department's incident tracker. The addresses were then geocoded with
+                using the batch geocoder API provided by the US Census Bureau. Any address that could not
+                be geolocated was removed from the data set. This typically included incidents 
+                that occured at the intersection of two roads. These data
+                were then geolocated to the Ward of interest within the City of Winston Salem.
+                The data were then matched to demographic data available from the American Community Survey.",
+                h2("Data Sources"),
+                a("Winston-Salem Police Department Incident Tracker", 
+                  target = "_blank", 
+                  href = paste0("http://www.cityofws.org/crimestats/txt/WSPD0101.txt")),
+                br(),
+                a("Census Bureau Geocoder", 
+                     target = "_blank", 
+                     href = paste0("https://www.census.gov/geo/maps-data/data/geocoder.html")
+                     ),
+                br(),
+                a("Census Bureau American Community Survey", 
+                  target = "_blank", 
+                  href = paste0("https://www.census.gov/programs-surveys/acs/")
+                ),
+                br(),
+                a("Winston-Salem Ward Definitions", 
+                     target = "_blank", 
+                     href = paste0("http://www.cityofws.org/departments/planning/gis/gis-data-sets-for-download")
+                ),
+                h2("Thanks!"),
+                box(uiOutput("wspd_logo"), width = 4),
+                box(uiOutput("census_logo"), width = 8)
+              )),
+      
+      # Descriptive Statistics
+      tabItem(tabName ="descr",
+              fluidRow(h2("This section investigates the different types of incidents that occur by Ward."), 
+                       h3("These can then be compared to the overall Winston-Salem area."),
+                       box(selectizeInput("ward", h3("Ward"), 
+                                      selected = "Northwest", choices = levels(as.factor(crime_dat$Ward))),
+                           width = 6),
+                       box(selectizeInput("month", h3("Month"), 
+                                      selected = "", choices = levels(as.factor(crime_dat$month))),
+                           width = 6),
+                       box(plotOutput("event_total"), width = 6),
+                       box(plotOutput("map_by_month"), width = 6),
+                       box(checkboxInput("demog", h4("See Demographics?")),width = 12),
+                       conditionalPanel("input.demog",
+                                        box(
+                                          h4("Summary by Ward"),
+                                          tableOutput("demog_table"), width = 4),
+                                        box(h4("Select the Point Ranges for More Details"),
+                                            plotOutput("demog_points", click = "plot_point",
+                                                       brush = brushOpts(id = "plot_brush")), width = 8),
+                                        box(h4("Selected Values"),
+                                            dataTableOutput("brush_info"), width = 12)
+                       ),
+                       
+                       box(downloadButton("download_descriptive", label = "Download All?"), width = 6)
+                       )),
+      
+      
+      # Analysis of When Things Happen
+      tabItem(tabName = "daytime",
+              fluidRow(h1("Analysis By Dates and Times"),
+                       box(plotOutput("day_of_week"), width = 6),
+                       box(plotOutput("day_of_week_trend"), width = 6),
+                       box(plotOutput("month_fig"), width = 12),
+                       box(plotOutput("ward_month_fig"), width = 12),
+                       box(plotOutput("plot_time"), width = 12),
+                       box(h4("Month Zoom"), width = 12),
+                       box(selectizeInput("time_month", h3("Month"), 
+                                              selected = "Jan", 
+                                          choices = levels(as.factor(crime_dat$month))),
+                               width = 6),
+                       box(sliderInput("date_slide", label = "Select Day",
+                                       value = 1,
+                                       min = 1,
+                                       max = 31,
+                                       step = 1), width =6),
+                       box(plotOutput("day_plot"), width = 12),
+                       br(),
+                       box(h4("Want to save the plots or the data?"), width = 12),
+                       box(downloadButton("download_all_date_time", label = "Plots"), width = 6),
+                       box(downloadButton("download_date_time_csv", label = "Data")), width = 6)),
+      # Analysis of Locations
+      tabItem(tabName = "geolocate",
+              fluidRow(
+                h1("Winston-Salem Ward Map"),
+                box(checkboxInput("points", h4("Add Incidents to Map", style = "color:red;")),
+                    width = 6),
+                conditionalPanel("input.points",
+                box(selectizeInput("map_month", h3("Month"), 
+                                   selected = "", choices = levels(as.factor(crime_dat$month))),
+                    width = 6)),
+                leafletOutput("map"),
+                h3("Below Find the Events That Have Occured in The Map Window"),
+                dataTableOutput("table")#,
+                #downloadButton("downloadCsv", "Download as CSV")
+              )
+      ),
+      
+      # Predictive Modeling
+      tabItem(tabName = "preds",
+              fluidRow(
+                withMathJax(),
+                h1("Preditive Modeling"),
+                box(withMathJax("This section requires some modeling. This section allows you to predict the likelhood of a given case type occuring in Ward. 
+                                The data on the demographics have been geocoded based on data from the American Community Survey.",
+                                em("Caution in that there is definitely colinearity and spatial autocorrelation in the predictors and the Wards.")),
+                    width = 12),
+                br(),
+                box(h4("Population Level Effects/ Predictors"),
+                tags$ul(
+                  tags$li("Percent White - The Percentage of Citizens Who are White"), 
+                  tags$li("Percent Poverty - The Percentage of Citizens Who are in Poverty")
+                ), width =6),
+                box(h4("Optional Group Effects"),
+                tags$ul(
+                  tags$li("Ward - The Ward in which the incident occured"), 
+                  tags$li("Tract - The census tract in which the incident occured")
+                ), width = 6),
+                box(selectizeInput("call", "Call Type", 
+                               selected = "ALARMS", choices = levels(as.factor(crime_dat$call_type))),
+                    width = 3),
+                box(selectizeInput("predictors", "Predictors", selected = "pctpov",
+                                   choices = c("Pct Poverty" = "pctpov", 
+                                               "Pct White"= "pctwhite",
+                                               "hour" = "hour"),
+                                   multiple = TRUE), width = 3),
+                box(checkboxInput("addgroupeffects", "Add Group Effects?"), width = 3),
+                box(conditionalPanel("input.addgroupeffects",
+                                 selectizeInput("groups", "Group Effects",
+                                                selected = "Ward",
+                                                choices = c("Ward" = "Ward",
+                                                            "Tract" = "tract"),
+                                                multiple = TRUE)), width = 3),
+                box(h3("Model Outputs"), width = 12),
+                box(uiOutput("equation"), width = 12),
+                box(verbatimTextOutput("model_fit"), width =6),
+                box(plotOutput("model_graph"), width =6),
+                box(h3("Model Data"), width =12),
+                box(dataTableOutput("data_to_model"), width = 12)
+              ))
+    )
+  )
+)
